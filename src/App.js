8227377262858abf57584aca454975d5cc9585e3
Patrick Richardson <prichardson@conversantmedia.com>
2019-10-19 16:19:30 -0500

    breaking into smaller components

--- src/App.js
+++ src/App.js
@@ -44,12 +44,16 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
+
     return (
       <div className="App">
-        <form>
-          <input type="text" onChange={this.onSearchChange} />
-        </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => (
+        {/* <form>
+          <input type="text" value={searchTerm} onChange={this.onSearchChange} />
+        </form> */}
+        <Search value={searchTerm} onChange={this.onSearchChange} />
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        {/* {list.filter(isSearched(searchTerm)).map(item => (
           <div key={item.objectID}>
             <span href={item.url}>{item.title}</span>
             <span>{item.author}</span>
@@ -57,6 +61,41 @@ class App extends Component {
             <span>{item.points}</span>
             <span>
               <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                Dismiss
+              </button>
+            </span>
+          </div>
+        ))} */}
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input type="text" value={value} onChange={onChange} />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => (
+          <div key={item.objectID}>
+            <span href={item.url}>{item.title}</span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button onClick={() => onDismiss(item.objectID)} type="button">
                 Dismiss
               </button>
             </span>
